b8b51b14d3904795c4861420d928cb0a3a747f5e
Markus Boegl <markus.boegl@student.tuwien.ac.at>
2010-01-24 13:15:26 +0000

    finished code cleaning and added some comments

--- include/BridgeRule.h
+++ include/BridgeRule.h
@@ -1,3 +1,34 @@
+/* dlvhex-mcs-equilibrium-plugin
+ * Calculate Equilibrium Semantics of Multi Context Systems in dlvhex
+ *
+ * Copyright (C) 2009,2010  Markus Boegl
+ * 
+ * This file is part of dlvhex-mcs-equilibrium-plugin.
+ *
+ * dlvhex-mcs-equilibrium-plugin is free software; 
+ * you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * dlvhex-mcs-equilibrium-plugin is distributed in the hope that it will
+ *  be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with dlvhex-dlplugin; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+
+/**
+ * @file   BridgeRule.h
+ * @author Markus Boegl
+ * @date   Sun Jan 24 13:32:05 2010
+ * 
+ * @brief  BridgeRule element for Parsing the Input file
+ */
 #ifndef _DLVHEX_MCSEQUILIBRIUM_BRIDGERULE_H_
 #define _DLVHEX_MCSEQUILIBRIUM_BRIDGERULE_H_
 
