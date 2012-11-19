c19c17cfd7dd93213a3cced2f25a5d058135b728
Christoph Redl <redl@kr.tuwien.ac.at>
2012-11-19 12:55:37 +0100

    update benchmark script

--- examples/benchmark_single.sh
+++ examples/benchmark_single.sh
@@ -1,15 +1,18 @@
 # $1: instance
-# $2: confstr
-# $3: timeout
+# $2: timeout
+
+export LD_LIBRARY_PATH=/mnt/lion/home/redl/local/lib
+export PATH=$PATH:/mnt/lion/home/redl/local/bin
+
+confstr="--solver=genuinegc --flpcheck=explicit -n=1;--solver=genuinegc --flpcheck=explicit -n=1 --extlearn"
 
 # default parameters
-if [ $# -le 2 ]; then
+if [ $# -le 1 ]; then
 	echo "Error: invalid parameters"
 	exit 1
 else
 	instance=$1
-	confstr=$2
-	to=$3
+	to=$2
 fi
 
 # split configurations
@@ -18,7 +21,6 @@ header="#size"
 i=0
 for c in "${confs[@]}"
 do
-	timeout[$i]=0
 	header="$header   \"$c\""
 	let i=i+1
 done
@@ -32,14 +34,8 @@ i=0
 for c in "${confs[@]}"
 do
 	echo -ne -e " "
-	# if a configuration timed out, then it can be skipped for larger instances
-	if [ ${timeout[$i]} -eq 0 ]; then
-		output=$(timeout 300 dlvhex2 $c --plugindir=../../src/.libs --ieenable --iemode=expl --ieexplain=E $instance --silent | wc -l)
-		if [[ $? == 124 ]]; then
-			output="---"
-			timeout[$i]=1
-		fi
-	else
+	output=$(timeout $to dlvhex2 $c --plugindir=../../src/.libs --ieenable --iemode=expl --ieexplain=E $instance --silent | wc -l)
+	if [[ $? == 124 ]]; then
 		output="---"
 	fi
 	echo -ne $output
